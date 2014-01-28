dace43ab6cb3bc5f8b695bedff2be193a702f630
Monsoon <monsoon@typhoon.fnal.gov>
2014-01-27 19:11:16 -0600

    Base set of voltages that work reasonably fine with d3-01 and d3-03.

---
+++ voltages_del_kun.sh
@@ -0,0 +1,72 @@
+pan att set SL3_A_V1_HiDac 3
+pan att set SL3_B_V1_HiDac 3
+pan att set SL3_C_V1_HiDac 3
+pan att set SL3_A_V2_HiDac 3
+pan att set SL3_B_V2_HiDac 3
+pan att set SL3_C_V2_HiDac 3
+pan att set SL3_A_V3_HiDac 3
+pan att set SL3_B_V3_HiDac 3
+pan att set SL3_C_V3_HiDac 3
+pan att set SL3_A_TG_HiDac 2
+pan att set SL3_B_TG_HiDac 2
+pan att set SL3_C_TG_HiDac 2
+
+pan att set SL3_A_V1_LowDac 0
+pan att set SL3_B_V1_LowDac 0
+pan att set SL3_C_V1_LowDac 0
+pan att set SL3_A_V2_LowDac 0
+pan att set SL3_B_V2_LowDac 0
+pan att set SL3_C_V2_LowDac 0
+pan att set SL3_A_V3_LowDac 0
+pan att set SL3_B_V3_LowDac 0
+pan att set SL3_C_V3_LowDac 0
+pan att set SL3_A_TG_LowDac 0.5
+pan att set SL3_B_TG_LowDac 0.5
+pan att set SL3_C_TG_LowDac 0.5
+
+
+pan att set SL3_A_H1L_HiDac 4
+pan att set SL3_B_H1L_HiDac 4
+pan att set SL3_C_H1L_HiDac 4
+pan att set SL3_A_H1U_HiDac 4
+pan att set SL3_B_H1U_HiDac 4
+pan att set SL3_C_H1U_HiDac 4
+pan att set SL3_A_H3L_HiDac 4
+pan att set SL3_B_H3L_HiDac 4
+pan att set SL3_C_H3L_HiDac 4
+pan att set SL3_A_H3U_HiDac 4
+pan att set SL3_B_H3U_HiDac 4
+pan att set SL3_C_H3U_HiDac 4
+
+pan att set SL3_A_H2_HiDac 4
+pan att set SL3_B_H2_HiDac 4
+pan att set SL3_C_H2_HiDac 4
+pan att set SL3_A_H2_LowDac 0
+pan att set SL3_B_H2_LowDac 0
+pan att set SL3_C_H2_LowDac 0
+
+pan att set SL3_A_H1L_LowDac 0
+pan att set SL3_B_H1L_LowDac 0
+pan att set SL3_C_H1L_LowDac 0
+pan att set SL3_A_H1U_LowDac 0
+pan att set SL3_B_H1U_LowDac 0
+pan att set SL3_C_H1U_LowDac 0
+pan att set SL3_A_H3L_LowDac 0
+pan att set SL3_B_H3L_LowDac 0
+pan att set SL3_C_H3L_LowDac 0
+pan att set SL3_A_H3U_LowDac 0
+pan att set SL3_B_H3U_LowDac 0
+pan att set SL3_C_H3U_LowDac 0
+
+pan att set SL3_A_SW_HiDac 2.6
+pan att set SL3_B_SW_HiDac 2.6
+pan att set SL3_C_SW_HiDac 2.6
+pan att set SL3_A_SW_LowDac 0
+pan att set SL3_B_SW_LowDac 0
+pan att set SL3_C_SW_LowDac 0
+pan att set SL5_CCD_VOG 2.6
+
+pan att set H_OVERLAP 20
+pan att set SW_WIDTH 20
+pan att set  POST_H_CLK 70
+#pan att set INTEG_WIDTH 8
