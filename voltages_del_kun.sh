bc58a031ea0a51f4aeae4256d3e6dff9d9a7c497
Monsoon <monsoon@typhoon.fnal.gov>
2014-01-28 19:06:05 -0600

    optimized for left amp. of d3-05

--- voltages_del_kun.sh
+++ voltages_del_kun.sh
@@ -1,12 +1,12 @@
-pan att set SL3_A_V1_HiDac 3
-pan att set SL3_B_V1_HiDac 3
-pan att set SL3_C_V1_HiDac 3
-pan att set SL3_A_V2_HiDac 3
-pan att set SL3_B_V2_HiDac 3
-pan att set SL3_C_V2_HiDac 3
-pan att set SL3_A_V3_HiDac 3
-pan att set SL3_B_V3_HiDac 3
-pan att set SL3_C_V3_HiDac 3
+pan att set SL3_A_V1_HiDac 5
+pan att set SL3_B_V1_HiDac 5
+pan att set SL3_C_V1_HiDac 5
+pan att set SL3_A_V2_HiDac 5
+pan att set SL3_B_V2_HiDac 5
+pan att set SL3_C_V2_HiDac 5
+pan att set SL3_A_V3_HiDac 5
+pan att set SL3_B_V3_HiDac 5
+pan att set SL3_C_V3_HiDac 5
 pan att set SL3_A_TG_HiDac 2
 pan att set SL3_B_TG_HiDac 2
 pan att set SL3_C_TG_HiDac 2
@@ -25,18 +25,18 @@ pan att set SL3_B_TG_LowDac 0.5
 pan att set SL3_C_TG_LowDac 0.5
 
 
-pan att set SL3_A_H1L_HiDac 4
-pan att set SL3_B_H1L_HiDac 4
-pan att set SL3_C_H1L_HiDac 4
-pan att set SL3_A_H1U_HiDac 4
-pan att set SL3_B_H1U_HiDac 4
-pan att set SL3_C_H1U_HiDac 4
-pan att set SL3_A_H3L_HiDac 4
-pan att set SL3_B_H3L_HiDac 4
-pan att set SL3_C_H3L_HiDac 4
-pan att set SL3_A_H3U_HiDac 4
-pan att set SL3_B_H3U_HiDac 4
-pan att set SL3_C_H3U_HiDac 4
+pan att set SL3_A_H1L_HiDac 5
+pan att set SL3_B_H1L_HiDac 5
+pan att set SL3_C_H1L_HiDac 5
+pan att set SL3_A_H1U_HiDac 5
+pan att set SL3_B_H1U_HiDac 5
+pan att set SL3_C_H1U_HiDac 5
+pan att set SL3_A_H3L_HiDac 5
+pan att set SL3_B_H3L_HiDac 5
+pan att set SL3_C_H3L_HiDac 5
+pan att set SL3_A_H3U_HiDac 5
+pan att set SL3_B_H3U_HiDac 5
+pan att set SL3_C_H3U_HiDac 5
 
 pan att set SL3_A_H2_HiDac 4
 pan att set SL3_B_H2_HiDac 4
@@ -58,9 +58,9 @@ pan att set SL3_A_H3U_LowDac 0
 pan att set SL3_B_H3U_LowDac 0
 pan att set SL3_C_H3U_LowDac 0
 
-pan att set SL3_A_SW_HiDac 2.6
-pan att set SL3_B_SW_HiDac 2.6
-pan att set SL3_C_SW_HiDac 2.6
+pan att set SL3_A_SW_HiDac 3.0
+pan att set SL3_B_SW_HiDac 3.0
+pan att set SL3_C_SW_HiDac 3.0
 pan att set SL3_A_SW_LowDac 0
 pan att set SL3_B_SW_LowDac 0
 pan att set SL3_C_SW_LowDac 0
